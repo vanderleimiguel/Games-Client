5bdba6d942055f5752a5a465bd0c63af505ad51e
vanderlei miguel <vanderleimiguel@hotmail.com>
2023-01-20 07:32:52 -0300

    feat: form login

--- client/src/components/molecules/login-form/login-form.tsx
+++ client/src/components/molecules/login-form/login-form.tsx
@@ -1,14 +1,26 @@
+import { FormEvent } from "react";
 import { FormInput } from "../../atoms/input";
 import { StyledForm, StyledLoginForm } from "./styles";
 
+const error = false;
+
 export function LoginForm() {
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const loginPayload = {
+      email: e.currentTarget.email.value,
+      password: e.currentTarget.password.value,
+    };
+    console.log(loginPayload);
+  }
+
   return (
     <StyledLoginForm>
       <h2>Login</h2>
       <StyledForm onSubmit={handleSubmit} error={error}>
-        <input />
-        <input />
-        <button>Login</button>
+        <input placeholder="Seu email" name="email" />
+        <input placeholder="Sua senha" name="password" />
+        <button type="submit">Login</button>
       </StyledForm>
     </StyledLoginForm>
   );
